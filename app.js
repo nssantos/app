fd5328630fd2d24708616ec4174ce27b0cc62083
Natanael Souza dos Santos <nss@WINDOWS.CIN.UFPE.BR>
2016-09-23 16:04:11 -0300

    unused files was removed and the code and folders structure was organized as angular standard structure

--- app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("elcomaApp", ['ngRoute']);
- app.config(function($routeProvider){
-	$routeProvider.when('/ola', {
-		controller: 'MainController',
-		templateUrl: 'views/timeline.html'
-	}).otherwise({
-		redirectTo: '/'
-	});
-}); 
